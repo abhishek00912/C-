3cb2991eecaad54f35f104e4a3c82c17198d1311
abhishek00912 <abhishekkumaryadav052@gmail.com>
2025-07-21 17:03:48 +0530

    Add file reading example (file2_read.cpp)

---
+++ FILE-HANDLING/file2_read.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+
+
+    ifstream fin;
+
+    //file ko open karo
+    // than read  karo
+
+    fin.open("zoom.txt");
+
+    // fr read karo 
+
+    char c;
+    c = fin.get();
+
+    while(!fin.eof())
+    {
+     cout << c;
+    c =  fin.get(); 
+    }
+
+
+fin.close();
+
+    return 0;
+}
